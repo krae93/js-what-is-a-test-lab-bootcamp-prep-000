8302ff4cc2a8bf7f0466ea05a220a44deb07028e
Kelly Rae Ramirez <k.rae1993@yahoo.com>
2018-04-05 18:42:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 // write your code below!
 var name = "Susan"
 var height = "74 < 40"
-if(height == "74 > 40") {
-  "74 is greater than 40";
-} else {
-  "74 is less than 40";
-}
 
 var message = `is ${height} inches tall`
 
