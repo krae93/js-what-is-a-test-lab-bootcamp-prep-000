1ca7b0f631178383d89fb1998e6b23d6941509fa
Kelly Rae Ramirez <k.rae1993@yahoo.com>
2018-04-05 18:59:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var height = 74 < 40
 if(74 > 40){
   console.log("74 is greater than 40")
 }else{
-  console.log("74 is greather than 40")
+  console.log("74 is less than 40")
 }
 
 var message = `is ${height} inches tall`
